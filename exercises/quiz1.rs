5ce78ae3028d4aaccab462fc2d20be9e86ac5325
suren <suren-m>
2020-12-26 01:51:21 +0000

    solved initial problems

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,8 +10,13 @@
 // I AM NOT DONE
 
 // Put your function here!
-// fn ..... {
-
+fn calculate_apple_price(count: i32) -> i32 {
+    if count > 40 {
+        count
+    } else {
+        count * 2
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
